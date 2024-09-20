725499705f4f75961fa2f7d165b30f9c4de467c4
Konstantinos Gatis <kgatis@iknowhow.com>
2024-09-20 13:01:30 +0300

    script and env changes

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,13 @@ import server from './server'
 import * as dotenv from 'dotenv'
 import path from 'path'
 
-const environment = process.env.NODE_ENV.trim() || 'development';
-dotenv.config({ path:  path.join(__dirname, `../.env.${environment}`)});
+// const environment = process.env.NODE_ENV.trim() || 'development';
+// dotenv.config({ path:  path.join(__dirname, `../.env.${environment}`)});
 
 const port = 3000;
 
 server.listen(port, () => {
     console.log(process.env.DATABASE_URL)
-    console.log(environment)
     console.log(`Server is running on http://localhost:${port}`);
 });
 
