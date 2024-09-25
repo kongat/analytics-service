0d3594f9cb10ffc2e1115a7efe65111ac007fa8a
Konstantinos Gatis <kgatis@iknowhow.com>
2024-09-25 14:20:20 +0300

    change port for development

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,9 @@ import path from 'path'
 // const environment = process.env.NODE_ENV.trim() || 'development';
 // dotenv.config({ path:  path.join(__dirname, `../.env.${environment}`)});
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-    console.log(process.env.DATABASE_URL)
     console.log(`Server is running on http://localhost:${port}`);
 });
 
